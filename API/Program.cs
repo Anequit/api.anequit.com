782b932f0c0df135f3c23ca491636b04fb671b25
Bailey <Anequit@gmail.com>
2023-06-26 11:04:39 -0400

    Add Services for status reporting

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,8 @@ internal class Program
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
+        builder.Services.AddHostedService<StatusReporting>();
+        builder.Services.AddHttpClient();
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
