48c1e5ccf6029f16aa715afffe418b35729b8a92
Bailey <Anequit@gmail.com>
2023-06-26 11:02:55 -0400

    Add StatusReporting

---
+++ API/Services/StatusReporting.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Services;
+
+public class StatusReporting : IHostedService
+{
+    private IHttpClientFactory _httpClientFactory;
+    private IConfiguration _configuration;
+
+    public StatusReporting(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        while(!cancellationToken.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+
+            using(HttpClient client = _httpClientFactory.CreateClient())
+            {
+                HttpResponseMessage response = await client.PostAsJsonAsync(_configuration["ReportingWebhook"], new Dictionary<string, string>()
+                {
+                    {
+                        "username", "API Report"
+                    },
+                    {
+                        "content", "Test"
+                    }
+                }, cancellationToken: cancellationToken);
+            }
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
