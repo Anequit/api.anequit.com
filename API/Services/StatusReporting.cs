1267d5b3f8b587a977e9c3be21825729d1230909
Bailey <Anequit@gmail.com>
2023-07-01 02:56:00 -0400

    Increase report delay

--- API/Services/StatusReporting.cs
+++ API/Services/StatusReporting.cs
@@ -24,7 +24,7 @@ public class StatusReporting : IHostedService
     {
         while (!cancellationToken.IsCancellationRequested)
         {
-            await Task.Delay(TimeSpan.FromMinutes(1), cancellationToken);
+            await Task.Delay(TimeSpan.FromMinutes(5), cancellationToken);
 
             using (HttpClient client = _httpClientFactory.CreateClient())
             {
